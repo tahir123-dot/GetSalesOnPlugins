597f32ea0a46ac93f4af8db27be11fe84d62c3e3
Tahir Rashid <tahirbhai736333@gmail.com>
2025-08-03 13:57:38 +0500

    added dotenv for configuration

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,27 +1,25 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    
     await mongoose.connect(process.env.MONGO_URL);
 
     console.log("MongoDB Connected Successfully");
   } catch (error) {
     console.error("Initial MongoDB Connection Error:", error.message);
-    process.exit(1); 
+    process.exit(1);
   }
 
-  
   mongoose.connection.on("error", (err) => {
     console.error("MongoDB Runtime Error:", err);
   });
 
- 
   mongoose.connection.on("disconnected", () => {
     console.warn("MongoDB Disconnected");
   });
 
-  
   process.on("SIGINT", async () => {
     await mongoose.connection.close();
     console.log("MongoDB connection closed due to app termination");
